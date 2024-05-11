7ccb10d858c4133cd1b3dfce4e78bc7cd0ab114c
Igor Warszawski <igorwarszawski3@gmail.com>
2024-05-11 16:02:45 +0200

    Stuff

--- util.h
+++ util.h
@@ -34,9 +34,4 @@ extern pthread_mutex_t stateMut;
 /* zmiana stanu, obwarowana muteksem */
 void changeState( state_t );
 
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-
 #endif
--- watek_komunikacyjny.cpp
+++ watek_komunikacyjny.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 #include "watek_komunikacyjny.h"
+#include <algorithm>
+
 
 /* wątek komunikacyjny; zajmuje się odbiorem i reakcją na komunikaty */
 void *startKomWatek(void *ptr)
@@ -12,7 +14,7 @@ void *startKomWatek(void *ptr)
 	debug("czekam na recv");
         MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         pthread_mutex_lock(&lamport_clock_mutex);
-        lamport_clock = max(lamport_clock, pakiet.ts) + 1;
+        lamport_clock = std::max(lamport_clock, pakiet.ts) + 1;
         pthread_mutex_unlock(&lamport_clock_mutex);
 
 
