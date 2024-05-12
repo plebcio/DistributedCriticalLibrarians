de1666d7daaff2827bac0d71852b6191b73af028
Igor Warszawski <igorwarszawski3@gmail.com>
2024-05-12 14:10:18 +0200

    Code up REST state

--- util.h
+++ util.h
@@ -33,10 +33,10 @@ extern MPI_Datatype MPI_PAKIET_T;
 void inicjuj_typ_pakietu();
 
 /* wysyłanie pakietu, skrót: wskaźnik do pakietu (0 oznacza stwórz pusty pakiet), do kogo, z jakim typem */
-void sendPacket(packet_t *pkt, int destination, mess_t tag);
+void sendPacket(packet_t *pkt, int destination, int tag);
 
 
-void broadcastPacket(packet_t *pkt, mess_t tag, int ts);
+void broadcastPacket(packet_t *pkt, int tag, int ts);
 
 
 
